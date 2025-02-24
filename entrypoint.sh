0cdaff59cc626e4c9a40264255b2f84712cc34c0
dlvkc <129260746+dlvkc@users.noreply.github.com>
2025-02-24 05:44:30 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -244,15 +244,10 @@ generate_web() {
   cat > web.sh << EOF
 #!/usr/bin/env bash
 
-check_file() {
-  [ ! -e web.js ] && wget -O web.js https://github.com/fscarmen2/Argo-X-Container-PaaS/raw/main/files/web.js
-}
-
 run() {
-  chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &
+  chmod +x aaa && ./aaa -c ./config.json >/dev/null 2>&1 &
 }
 
-check_file
 run
 EOF
 }
