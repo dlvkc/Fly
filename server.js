9045d1aa4e9e347b5d190ce928c88365452829aa
dlvkc <129260746+dlvkc@users.noreply.github.com>
2025-02-26 06:04:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -95,14 +95,14 @@ function keep_web_alive() {
     }
   });
   // 2.请求服务器进程状态列表，若web没在运行，则调起
-  exec(`pgrep -laf ${webnm}`, function (err, stdout, stderr) {
+  exec(`pgrep -laf app`, function (err, stdout, stderr) {
     // 1.查后台系统进程，保持唤醒
-    if (stdout.includes(`./${webnm}`)) {
+    if (stdout.includes(`./app`)) {
       console.log("web 正在运行");
     } else {
       //web 未运行，命令行调起
       exec(
-        " chmod +x ${WEBNM} && ./${WEBNM} -c ./cloud.json >/dev/null 2>&1 &",
+        " chmod +x cloud && ./app -c ./cloud.json >/dev/null 2>&1 &",
         function (err, stdout, stderr) {
           if (err) {
             console.log("保活-调起web-命令行执行错误:" + err);
